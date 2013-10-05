e7f39d6b29464bddbd50097d80b10a80f7946fc8
Alfio Zappala <azappala@azaptree.com>
2013-10-05 06:29:14 -0400

    setup initial project structure

---
+++ test/log-manager.js
@@ -0,0 +1,47 @@
+/**
+ * Copyright [2013] [runrightfast.co]
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+'use strict';
+var expect = require('chai').expect;
+
+var config = require('../lib').config;
+
+describe('LogManager', function() {
+
+	it('can list the log files', function() {
+
+	});
+
+	it('can gzip old logs', function() {
+
+	});
+
+	it('can delete old log files', function() {
+
+	});
+
+	it('can tail a log file', function() {
+
+	});
+
+	it('can tail -n a log file', function() {
+
+	});
+
+	it('can head -n a log file', function() {
+
+	});
+});
